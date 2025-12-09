de0dde2b78c9d4ec61e959cbffb41e85c65fb27a
thulii <Taras.Chulii@gmail.com>
2025-12-09 14:49:54 +0200

    add powershell script

---
+++ ps-script.ps1
@@ -0,0 +1,12 @@
+﻿Clear-Host
+
+Write-Host "The practice started"
+Write-Host "====================`n"
+
+$counter = 14
+
+while ($counter -gt 0) {
+
+    Write-Host "The counter is $counter" 
+    $counter--
+}
